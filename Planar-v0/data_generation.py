5eb89758c17e62af4f443cfa8d0d4e3beb880e8c
nilam-t <141142169+nilam-t@users.noreply.github.com>
2023-09-05 10:19:23 +0200

    hybrid_nn_pde_scripts

---
+++ Planar-v0/data_generation.py
@@ -0,0 +1,138 @@
+# ----------------------------------------------------------------------------
+#
+# Phiflow chemical reaction solver framework
+# Copyright 2023 Nilam T, Nils Thuerey
+#
+# This program is free software, distributed under the terms of the
+# Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Data generation
+#
+# ----------------------------------------------------------------------------
+
+import os, sys, logging, argparse, pickle, glob, random, distutils.dir_util
+from solver_class import SpEnergy, SpFluid
+
+fuel_type = 'methane'
+if fuel_type == 'methane':
+    A_m, n_m, E_m = 1.1E7, 0, 83600
+    hk, cp = 5.01E7, 1450
+    Wf, Wo, Wp = 0.016, 0.032, 0.062
+    Vf, Vo = -1, -2
+elif fuel_type == 'propane':
+    A_m, n_m, E_m = 2.75e8, 0, 130317
+    hk, cp = 4.66E7, 1300
+    Wf, Wo, Wp = 0.044, 0.032, 0.062
+    Vf, Vo = -1, -5
+
+log = logging.getLogger()
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
+
+params = {}
+parser = argparse.ArgumentParser(description='Parameter Parser', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--gpu',             default='0',             help='visible GPUs')
+parser.add_argument('--cuda',            action='store_true',     help='enable CUDA for solver')
+parser.add_argument('-o', '--output',    default=None,            help='path to an output directory')
+parser.add_argument('-t', '--simsteps',  default=300, type=int,  help='simulation steps: an epoch')
+parser.add_argument('-s', '--skipsteps', default=0, type=int,   help='skip first steps; (vortices may not form)')
+parser.add_argument('-r', '--res',       default=32, type=int,    help='resolution of the reference axis')
+parser.add_argument('--er',              default=1.0, type=float, help='fuel-air equivalence ratio')
+parser.add_argument('-b', '--bx',       default=0.05, type=float,   help='length of combustion chamber (box)')
+parser.add_argument('--seed',            default=0, type=int,     help='seed for random number generator')
+sys.argv += ['--' + p for p in params if isinstance(params[p], bool) and params[p]]
+pargs = parser.parse_args()
+params.update(vars(pargs))
+
+os.environ['CUDA_VISIBLE_DEVICES'] = params['gpu']
+
+if params['cuda']: from phi.tf.tf_cuda_pressuresolver import CUDASolver
+
+from phi.tf.flow import *
+import phi.tf.util
+
+import tensorflow as tf
+from tensorflow import keras
+
+random.seed(params['seed'])
+np.random.seed(params['seed'])
+tf.compat.v1.set_random_seed(params['seed'])
+
+class Chemical_reaction(SpEnergy):
+    def __init__(self, pressure_solver=GeometricCG()):
+        SpEnergy.__init__(self, pressure_solver)
+
+
+    def step(self, smoke, react, dt=1.0, gravity=Gravity()):
+        density = (1.0 / 8.314) * smoke.pressure * (((smoke.Yf * (1 / Wf) + smoke.Yo * (1 / Wo) + (1 - smoke.Yf - smoke.Yo) * (1 / Wp)) * smoke.temperature) ** (-1))
+        if react == 1:
+            Q1 = A_m * (density * smoke.Yf/Wf) * ((density*smoke.Yo/Wo)**0.5) * keras.backend.exp(-E_m * ((8.314 * smoke.temperature.data) ** (-1)))
+        else:
+            Q1 = 1
+        wkf = Q1 * (Vf) * Wf
+        wko = Q1 * (Vo) * Wo
+
+        wt = hk * (wkf)
+        smoke = smoke.copied_with(velocity = 0.0, Wkf = wkf, Wko = wko, Wt = wt)
+        return super().step(fluid=smoke, dt=dt, obstacles=(), gravity=gravity, density_effects=(), velocity_effects=())
+
+rst = SpFluid(Domain([params['res'], params['res']], box=AABox(0, [params['bx'], params['bx']]), boundaries=CLOSED), buoyancy_factor=0)
+
+Zf = 1/(1 + (4*4.29/params['er']))
+Zo = 1-Zf
+t1 = np.linspace(-15, 8, params['res'])
+temp1 = 1200 + 800 * np.tanh(t1)
+yf = Zf * (-np.tanh(t1) + 1) / 2
+yo = Zo * (-np.tanh(t1) + 1) / 2
+
+# sigmoid
+for idx1 in range(params['res']):
+    rst.Yf.data[:, idx1, :, :] = yf[idx1]
+    rst.Yo.data[:, idx1, :, :] = yo[idx1]
+    rst.temperature.data[:, idx1, :, :] = temp1[idx1]
+
+Yf0, Yo0 = CenteredGrid(rst.Yf.data, rst.Yf.box), CenteredGrid(rst.Yo.data, rst.Yo.box)
+T0 = CenteredGrid(rst.temperature.data, rst.temperature.box)
+rst = rst.copied_with(temperature= T0, Yf = Yf0, Yo = Yo0)
+# phiflow scene
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+tf_session = tf.Session(config=config)
+
+scene = Scene.create(directory=params['output'])
+sess  = Session(scene, session=tf_session)
+
+log.addHandler(logging.FileHandler(os.path.normpath(scene.path)+'/run.log'))
+log.info(params)
+log.info('tensorflow-{} ({}, {}); keras-{} ({})'.format(tf.__version__, tf.sysconfig.get_include(), tf.sysconfig.get_lib(), keras.__version__, keras.__path__))
+
+if params['output']:
+    with open(os.path.normpath(scene.path)+'/params.pickle', 'wb') as f: pickle.dump(params, f)
+
+simulator = Chemical_reaction()
+tf_rst_in = phi.tf.util.placeholder_like(rst)
+tf_rst = simulator.step(tf_rst_in, react=1, dt=0.000001)
+
+if params['skipsteps']==0 and params['output'] is not None:
+
+    scene.write(
+        [rst.temperature, rst.Yf, rst.Yo, rst.pressure],
+        ['temp', 'Yf', 'Yo', 'pressure'],
+        0
+    )
+
+for i in range(1, params['simsteps']):
+
+    my_feed_dict = {tf_rst_in: rst}
+    rst = sess.run(tf_rst, my_feed_dict)
+
+    log.info('Step {:06d}'.format(i))
+    if params['skipsteps']<i and params['output'] is not None:
+        scene.write(
+            [rst.temperature, rst.Yf, rst.Yo, rst.pressure],
+            ['temp', 'Yf', 'Yo', 'pressure'],
+            i
+        )
+
---
+++ Planar-v0/hybrid_nn_pde_apply_v1.py
@@ -0,0 +1,197 @@
+# ----------------------------------------------------------------------------
+#
+# Phiflow chemical reaction solver framework
+# Copyright 2020 Nilam T, Nils Thuerey
+#
+# This program is free software, distributed under the terms of the
+# Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Apply correction network model
+#
+# ----------------------------------------------------------------------------
+
+import os, sys, logging, argparse, pickle
+from solver_class import SpEnergy, SpFluid
+
+fuel_type = 'methane'
+if fuel_type == 'methane':
+    hk, cp = 5.01E7, 1450
+    Wf, Wo, Wp = 0.016, 0.032, 0.062
+    Vf, Vo = -1, -2
+elif fuel_type == 'propane':
+    hk, cp = 4.66E7, 1300
+    Wf, Wo, Wp = 0.044, 0.032, 0.062
+    Vf, Vo = -1, -5
+
+log = logging.getLogger()
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
+
+params = {}
+parser = argparse.ArgumentParser(description='Parameter Parser', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--gpu',            default='0',                                  help='visible GPUs')
+parser.add_argument('-s', '--scale',    default=4, type=int,                          help='simulation scale for high-res')
+parser.add_argument('-r', '--res',      default=32, type=int,                         help='resolution of the reference axis')
+parser.add_argument('--testset',          default=None,              help='training; will load data from this simulation folder (no reaction)')
+parser.add_argument('--bx',       default=0.05, type=float,   help='length of combustion chamber (box)')
+parser.add_argument('-l', '--len',      default=100, type=int,                        help='length of the reference axis')
+parser.add_argument('-b', '--sbatch',   default=1, type=int,       help='size of a batch; when 10 simulations with the size of 5, 5 simulations are into two batches')
+parser.add_argument('--bIdx',   default=0, type=int,       help='batch index to compute simulation number')
+parser.add_argument('--er',             default=1.0, type=float,                      help='Equivalence ratio')
+parser.add_argument('--initTnr',         default=None,                                help='load no-reaction temperature  (e.g., nr_temp_0000.npz)')
+parser.add_argument('--initYfnr',         default=None,                               help='load no-reaction Yf (e.g., nr_Yf_0000.npz)')
+parser.add_argument('--initYonr',         default=None,                               help='load no-reaction Yf (e.g., nr_Yo_0000.npz)')
+parser.add_argument('--initVnr',         default=None,                               help='load no-reaction vel (e.g., nr_vel_0000.npz)')
+parser.add_argument('-t', '--simsteps', default=300, type=int,                        help='simulation steps')
+parser.add_argument('-o', '--output',   default='/tmp/phiflow/run',                   help='path to an output directory')
+parser.add_argument('--stats',          default='/tmp/phiflow/data/dataStats.pickle', help='path to datastats')
+parser.add_argument('--model',          default='/tmp/phiflow/tf/model.h5',           help='path to a tensorflow model')
+sys.argv += ['--' + p for p in params if isinstance(params[p], bool) and params[p]]
+pargs = parser.parse_args()
+params.update(vars(pargs))
+
+os.environ['CUDA_VISIBLE_DEVICES'] = params['gpu']
+
+from phi.flow import *
+
+import tensorflow as tf
+from tensorflow import keras
+
+def to_staggered(tensor_cen, box):
+    with tf.name_scope('to_staggered') as scope:
+        return StaggeredGrid(math.pad(tensor_cen, ((0,0), (0,1), (0,1), (0,0))), box=box)
+
+def get_zf_zo(eq):
+    Zf = 1/(1 + (4*4.29/eq))
+    Zo = 1-Zf
+    return Zf, Zo
+
+def to_feature(smokestates, E0):
+    # input feature; drop the unused edges of the staggered velocity grid making its dim same to the centered grid's
+    #print(len(smokestates))
+    with tf.name_scope('to_feature') as scope:
+        return math.concat(
+            [smokestates[j].temperature.data for j in range(len(smokestates))] +
+            [smokestates[j].Yf.data for j in range(len(smokestates))] +
+            [smokestates[j].Yo.data for j in range(len(smokestates))] +
+            [E0],  # equivalence ratio
+            axis=-1
+        )
+
+def mask_mass_fraction_new(tf_st_co_prd, tf_cv_md, e0, var_name, box):
+    y_final =  tf_cv_md
+    for d in range(y_final.data.shape[0]):
+        Zf, Zo = get_zf_zo(e0[d])
+        y_data = y_final.data[d:(d+1),:,:,:]
+        if var_name == 'Yf':
+            thres = Zf
+        elif var_name == 'Yo':
+            thres = Zo
+        if d == 0:
+            yd_data = np.clip(y_data, 0, thres)
+        else:
+            yd_data = np.concatenate((yd_data, np.clip(y_data, 0, thres)),axis=0)
+
+    return CenteredGrid(yd_data, box=box)
+
+class Chemical_reaction(SpEnergy):
+    def __init__(self, pressure_solver=GeometricCG()):
+        SpEnergy.__init__(self, pressure_solver)
+
+
+    def step(self, smoke, eq=1.0, dt=1.0, gravity=Gravity()):
+        Q1 = 1
+        wkf = Q1 * (Vf) * Wf
+        wko = Q1 * (Vo) * Wo
+
+        wt = hk * (wkf)
+        smoke = smoke.copied_with(velocity = 0.0, pressure = 101325, Wkf = wkf, Wko = wko, Wt = wt)
+        return super().step(fluid=smoke, dt=dt, obstacles=(), gravity=gravity, density_effects=(), velocity_effects=())
+
+
+# initialize
+st = SpFluid(Domain([params['res'], params['res']], box=AABox(0, [params['bx'], params['bx']]), boundaries=CLOSED), buoyancy_factor=0, batch_size=params['sbatch'])
+e0 = []
+for b in range(params['sbatch']):
+    sim_no = str(params['bIdx']*params['sbatch'] + b).zfill(6)
+    if int(params['bIdx']*params['sbatch'] + b) > 3:
+        sim_no = str(3).zfill(6)
+    initTnr = params['testset'] + '/sim_' + sim_no + '/temp_000000.npz'
+    initYfnr = params['testset'] + '/sim_' + sim_no + '/Yf_000000.npz'
+    initYonr = params['testset'] + '/sim_' + sim_no + '/Yo_000000.npz'
+    with open(params['testset'] + '/sim_' + sim_no + '/params.pickle', 'rb') as f:
+        params0 = pickle.load(f)
+
+    if b == 0:
+        T0 = read_zipped_array(initTnr)
+        Yf0 = read_zipped_array(initYfnr)
+        Yo0 = read_zipped_array(initYonr)
+        E0 = np.ones((1,32,32,1))*params0['er']
+
+    else:
+        T0 = np.concatenate((T0,read_zipped_array(initTnr)),axis=0)
+        Yf0 = np.concatenate((Yf0,read_zipped_array(initYfnr)), axis=0)
+        Yo0 = np.concatenate((Yo0, read_zipped_array(initYonr)), axis=0)
+        E0 = np.concatenate((E0, np.ones((1,32,32,1))*params0['er']), axis=0)
+    e0.append(params0['er'])
+
+print(T0.shape, Yf0.shape, E0.shape)
+st = [ st.copied_with(temperature=T0, Yf=Yf0, Yo=Yo0) for _ in range(1) ]  # NOTE: update according to the input feature!
+
+# extra field
+cv = st[0].staggered_grid(name="corr", value=0)
+
+# phiflow scene
+scene = Scene.create(directory=params['output'])
+print('output dir - ',params['output'])
+log.addHandler(logging.FileHandler(os.path.normpath(scene.path)+'/run.log'))
+log.info(params)
+log.info('tensorflow-{} ({}, {}); keras-{} ({})'.format(tf.__version__, tf.sysconfig.get_include(), tf.sysconfig.get_lib(), keras.__version__, keras.__path__))
+
+log.info(E0)
+if params['output']:
+    with open(os.path.normpath(scene.path)+'/params.pickle', 'wb') as f: pickle.dump(params, f)
+
+
+# load a tf model and stats used for data normalization
+with open(params['stats'], 'rb') as f: data_stats = pickle.load(f)
+log.info(data_stats)
+model = keras.models.load_model(params['model'])  # Fully convolutional, so we can use trained weights regardless the input dimension
+model.summary(print_fn=log.info)
+
+scene.write(
+    [st[-1].temperature, st[-1].Yf, st[-1].Yo, cv],
+    ['tempTf', 'YfTf', 'YoTf', 'corTf'],
+    0
+)
+
+simulator = Chemical_reaction()
+for i in range(1, params['simsteps']):
+    for j in range(len(st)-1):
+        st[j] = st[j+1]
+
+    st[-1] = simulator.step(st[-1], eq=E0, dt=0.000001)
+
+    inputf = (to_feature(st, E0) - [*(data_stats['mean'][0:3]), 0.0])/[
+        *(data_stats['std'][0:3]), 1.0]
+
+    cv_pred = model.predict(inputf)*([*(data_stats['std'][0:3])]) + ([*(data_stats['mean'][0:3])])
+    cv = cv_pred[:, :, :, 0]
+    cv_temp = CenteredGrid(cv.reshape((-1,32,32,1)), st[-1].temperature.box)
+    cv = cv_pred[:, :, :, 1]
+    cv_yf = CenteredGrid(cv.reshape((-1,32,32,1)), st[-1].Yf.box)
+    cv = cv_pred[:, :, :, 2]
+    cv_yo = CenteredGrid(cv.reshape((-1,32,32,1)), st[-1].Yo.box)
+    final_yf = mask_mass_fraction_new(st, cv_yf, e0, 'Yf', st[-1].Yf.box)
+    final_yo = mask_mass_fraction_new(st, cv_yo, e0, 'Yo', st[-1].Yo.box)
+
+    #st[-1] = st[-1].copied_with(temperature=st[-1].temperature + cv_temp, Yf = st[-1].Yf+cv_yf, Yo=st[-1].Yo+cv_yo)
+    st[-1] = st[-1].copied_with(temperature=cv_temp, Yf=final_yf, Yo=final_yo)
+
+    log.info('step {:06d}'.format(i))
+    scene.write(
+        [st[-1].temperature, st[-1].Yf, st[-1].Yo, cv],
+        ['tempTf', 'YfTf', 'YoTf', 'corTf'],
+        i
+    )
